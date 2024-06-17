11997e2cfaa891689808e2feda3344383a21b00b
kimdodo97 <eh4536@naver.com>
2024-06-17 23:49:28 +0900

    Feat : 사용자 권한 추가/조회 추가

--- src/main/java/com/example/store/repository/mapper/UserMapper.java
+++ src/main/java/com/example/store/repository/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.example.store.repository.mapper;
 
 import com.example.store.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -10,7 +12,9 @@ public interface UserMapper {
     Optional<User> findById(Long id);
     Optional<User> findByEmail(String email);
     Optional<User> findByEmailAndPassword(String email,String password);
+    List<String> findRoleByUserId(Long userId);
     Long save(User user);
+    void saveUserRole(@Param("userId") Long userId, @Param("role") String role);
     void update(User user);
     void delete(Long id);
     void deleteAll();
