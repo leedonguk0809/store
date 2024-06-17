576049a8956ae436560cf5517136fc4080a6846f
leedonguk0809 <ehddnr0481@naver.com>
2024-06-17 16:15:15 +0900

    Feat:orderitem 도메인 추가

---
+++ src/main/java/com/example/store/domain/OrderItem.java
@@ -0,0 +1,14 @@
+package com.example.store.domain;
+
+
+import lombok.Data;
+
+
+public class OrderItem {
+
+    private Long orderItemId;
+    private Long orderId;
+    private Long itemId;
+
+
+}
