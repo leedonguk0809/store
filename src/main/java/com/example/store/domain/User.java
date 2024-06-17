bcd8c4179618c7646119c31e2fb171575090cf8d
leedonguk0809 <ehddnr0481@naver.com>
2024-06-17 16:15:56 +0900

    Feat:user 도메인 추가

---
+++ src/main/java/com/example/store/domain/User.java
@@ -0,0 +1,17 @@
+package com.example.store.domain;
+
+
+import lombok.Data;
+
+
+public class User {
+
+    private Long userId;
+    private String name;
+    private String email;
+    private String password;
+    private String telNumber;
+    private String zipcode;
+    private String mainAddress;
+    private String detailAddress;
+}
