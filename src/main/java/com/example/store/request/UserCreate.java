32f59f08833ad6146cdc27a756419ed8e70759c9
kimdodo97 <eh4536@naver.com>
2024-06-17 17:50:10 +0900

    Feat : 사용자 생성 리퀘스트 추가

---
+++ src/main/java/com/example/store/request/UserCreate.java
@@ -0,0 +1,42 @@
+package com.example.store.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class UserCreate {
+
+    @NotBlank(message = "사용자 이메일 필수")
+    private String email;
+    
+    @NotBlank(message = "사용자 비밀번호 필수")
+    private String password;
+
+    @NotBlank(message = "사용자 이름은 필수")
+    private String name;
+
+    private String telNumber;
+
+    private String zipcode;
+
+    private String mainAddress;
+
+    private String detailAddress;
+
+    @Builder
+    public UserCreate(String email, String password, String name, String telNumber, String zipcode, String mainAddress, String detailAddress) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.telNumber = telNumber;
+        this.zipcode = zipcode;
+        this.mainAddress = mainAddress;
+        this.detailAddress = detailAddress;
+    }
+
+    public void validate(){
+
+    }
+}
