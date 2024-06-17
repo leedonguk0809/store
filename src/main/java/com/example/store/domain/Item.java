e1f327fe917950c20d05d0ea53a22c4769a401f5
leedonguk0809 <ehddnr0481@naver.com>
2024-06-17 16:16:09 +0900

    Feat:item 도메인 추가

---
+++ src/main/java/com/example/store/domain/Item.java
@@ -0,0 +1,14 @@
+package com.example.store.domain;
+
+
+import lombok.Data;
+
+
+public class Item {
+
+    private Long itemId;
+    private String name;
+    private Long price;
+    private Long stock;
+    private String info;
+}
