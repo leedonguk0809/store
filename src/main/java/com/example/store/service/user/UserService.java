232b08d91b21ef1df7375ce367e7f340f79daa2a
kimdodo97 <eh4536@naver.com>
2024-06-18 17:43:12 +0900

    Feat : 사용자 Get 메서드 추가

--- src/main/java/com/example/store/service/user/UserService.java
+++ src/main/java/com/example/store/service/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.store.service.user;
 
+import com.example.store.domain.User;
 import com.example.store.domain.UserStatus;
 import com.example.store.request.user.UserCreate;
 import com.example.store.request.user.UserEdit;
@@ -7,8 +8,8 @@ import com.example.store.request.user.UserEdit;
 public interface UserService {
     Long signup(UserCreate userCreate);
     Boolean validateDuplicate(String email);
+    User get(String email, String password);
     Long edit(Long userId,UserEdit postEdit);
-
     Long editUserStatus(Long id, UserStatus userStatus);
     void delete(Long userId);
 }
