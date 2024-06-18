aa8c232b67bbc898460d18540075b37aabe11bd3
kimdodo97 <eh4536@naver.com>
2024-06-19 01:25:49 +0900

    Feat : 사용자 이메일 존재 Mapper 추가

--- src/main/java/com/example/store/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/store/repository/UserRepositoryImpl.java
@@ -22,6 +22,7 @@ public class UserRepositoryImpl implements UserRepository{
         return userMapper.findByEmail(email);
     }
 
+
     @Override
     public Optional<User> findByEmailAndPassword(String email, String password) {
         return userMapper.findByEmailAndPassword(email,password);
@@ -30,6 +31,11 @@ public class UserRepositoryImpl implements UserRepository{
     @Override
     public List<String> findRoleByUserId(Long userId) {
         return userMapper.findRoleByUserId(userId);
+    }
+
+    @Override
+    public Boolean existsEmail(String email) {
+        return userMapper.existEmail(email);
     }
 
     @Override
