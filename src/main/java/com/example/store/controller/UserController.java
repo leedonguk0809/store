2a8d90a8db58e35c7358ca13c48e29ff4c1ac682
kimdodo97 <eh4536@naver.com>
2024-06-18 17:45:10 +0900

    Fix : 사용자 컨트롤러 회원가입 수정

--- src/main/java/com/example/store/controller/UserController.java
+++ src/main/java/com/example/store/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
