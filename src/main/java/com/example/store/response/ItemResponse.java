91f4e0434ff29fb2d954e9dbb9d69af68b2b9695
kimdodo97 <eh4536@naver.com>
2024-06-19 16:46:08 +0900

    Feat : 아이템 응답 클래스 추가

---
+++ src/main/java/com/example/store/response/ItemResponse.java
@@ -0,0 +1,33 @@
+package com.example.store.response;
+
+import com.example.store.domain.Item;
+import com.example.store.repository.item.ItemRepository;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ItemResponse {
+    private Long id;
+    private String name;
+    private Long price;
+    private String info;
+    private String profileImage;
+
+    @Builder
+    public ItemResponse(Long id, String name, Long price, String info, String profileImage) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.info = info;
+        this.profileImage = profileImage;
+    }
+
+    public static ItemResponse fromEntity(Item item){
+        return ItemResponse.builder()
+                .id(item.getItemId())
+                .name(item.getName())
+                .price(item.getPrice())
+                .info(item.getInfo())
+                .build();
+    }
+}
