500a3a0d9d82c22c36188253f82e43ef645f8f54
kimdodo97 <eh4536@naver.com>
2024-06-19 16:46:00 +0900

    Feat : 아이템 조회 클래스 추가

---
+++ src/main/java/com/example/store/request/item/ItemSearch.java
@@ -0,0 +1,27 @@
+package com.example.store.request.item;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ItemSearch {
+    private static final int MAX_SIZE = 2000;
+
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
+
+    @Builder.Default
+    private boolean asc = false;
+
+    @Builder.Default
+    private String keyword = "";
+
+    int blockLimit = 3;
+    public long getOffset(){
+        return (long)(Math.max(1,page)-1)*Math.min(size,MAX_SIZE);
+    }
+}
