073961a9bba892999556fb6d6a050bf3509930d8
kimdodo97 <eh4536@naver.com>
2024-06-19 16:44:44 +0900

    Feat : 사용자 로그인 클래스 자동로그인 필드 추가

--- src/main/java/com/example/store/request/user/UserLogin.java
+++ src/main/java/com/example/store/request/user/UserLogin.java
@@ -13,9 +13,11 @@ public class UserLogin {
     @NotBlank(message = "로그인 비밀번호 필수")
     private String password;
 
+    private boolean rememberMe;
     @Builder
-    public UserLogin(String email, String password) {
+    public UserLogin(String email, String password, boolean rememberMe) {
         this.email = email;
         this.password = password;
+        this.rememberMe = rememberMe;
     }
 }
