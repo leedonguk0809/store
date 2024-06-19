8a3b37639e1bdbbe7a5e356d7c36cc27ad3e2d20
kimdodo97 <eh4536@naver.com>
2024-06-19 16:49:03 +0900

    Feat : 사용자 View 컨트롤러 엔드포인트 추가 - 추후 정리

--- src/main/java/com/example/store/controller/UserViewController.java
+++ src/main/java/com/example/store/controller/UserViewController.java
@@ -1,14 +1,23 @@
 package com.example.store.controller;
 
+import com.example.store.common.config.UserPrincipal;
+import com.example.store.domain.User;
 import com.example.store.request.user.UserCreate;
-import org.springframework.http.ResponseEntity;
+import com.example.store.response.UserResponse;
+import com.example.store.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserViewController {
+    private final UserService userService;
     @GetMapping("/login-form")
     public String getLoginView(){
         return "auth/login";
@@ -18,5 +27,19 @@ public class UserViewController {
     public String getSignUpView(Model model, UserCreate userCreate){
         model.addAttribute("userCreate",userCreate);
         return "auth/signup";
+    }
+
+    @GetMapping("/mypage")
+    @PreAuthorize("hasRole('USER')")
+    public String getMain(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        Long userId = userPrincipal.getUserId();
+
+        User user = userService.get(userId);
+        UserResponse userResponse = UserResponse.fromEntity(user);
+        model.addAttribute("user",userResponse);
+
+        return "user/mypage";
     }
 }
