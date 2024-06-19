4688077823a40cb0d744ffb6c25be9e6486dea9c
kimdodo97 <eh4536@naver.com>
2024-06-19 16:46:33 +0900

    Feat : 아이템 맵퍼 페이징 조회 추가

--- src/main/java/com/example/store/repository/mapper/ItemMapper.java
+++ src/main/java/com/example/store/repository/mapper/ItemMapper.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 @Mapper
 public interface ItemMapper {
-
-
     void insertItem(Item item);
 
     Item findItemById(@Param("itemId") Long itemId);
 
     List<Item> findAllItems();
 
+    List<Item> findByPage(@Param("size") int size, @Param("offset") long offset,
+                          @Param("asc") boolean asc, @Param("keyword") String keyword);
     void updateItem(Item item);
 
     void deleteItem(@Param("itemId") Long itemId);
