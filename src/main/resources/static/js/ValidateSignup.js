a4b353a212f50ddfeae9d97b899c93d2f5ce80a3
kimdodo97 <eh4536@naver.com>
2024-06-19 17:40:46 +0900

    Feat : 로그인 중복 핸들러 추가

--- src/main/resources/static/js/ValidateSignup.js
+++ src/main/resources/static/js/ValidateSignup.js
@@ -1,3 +1,41 @@
+async function checkEmail(email) {
+    try {
+        const response = await fetch(`/check-email?email=${encodeURIComponent(email)}`);
+        console.log(response);
+        if (!response.status===200) {
+            throw new Error('서버에서 이메일 확인 요청에 실패했습니다.');
+        }
+        const data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.error('이메일 확인 요청 실패:', error);
+        return { status: 'error', message: '서버 오류가 발생했습니다.' };
+    }
+}
+
+document.getElementById('email-valid-btn').addEventListener('click', async () => {
+    const emailInput = document.getElementById('email-input').value;
+    const result = await checkEmail(emailInput);
+    handleCheckResult(result);
+});
+
+
+function handleCheckResult(result) {
+    const statusElement = document.getElementById('emailError');
+    console.log(result);
+    if (result.use === 'disable') {
+        statusElement.textContent = '중복된 이메일입니다.';
+        statusElement.style.color = 'red';
+    } else if (result.use === 'available') {
+        statusElement.textContent = '사용 가능한 이메일입니다.';
+        statusElement.style.color = 'green';
+    } else {
+        statusElement.textContent = '이메일 확인 중 오류가 발생했습니다.';
+        statusElement.style.color = 'orange';
+    }
+}
+
 document.addEventListener('DOMContentLoaded',function(){
     const password = document.getElementById('password')
     const passwordRetry = document.getElementById('passwordRetry')
@@ -5,6 +43,8 @@ document.addEventListener('DOMContentLoaded',function(){
 
     const telNumber = document.getElementById('telNumber');
     const telNumberErrorMessage = document.getElementById('telNumberError')
+
+
 
     function validatePassword() {
         let passwordInput = password.value.trim(); // 입력값을 얻고 앞뒤 공백을 제거
