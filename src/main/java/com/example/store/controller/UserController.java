2ad5765bfcb8e53240e4e840e3979da50a8d44ce
kimdodo97 <eh4536@naver.com>
2024-06-19 17:40:59 +0900

    Feat : 로그인 중복 엔드포인트 추가

--- src/main/java/com/example/store/controller/UserController.java
+++ src/main/java/com/example/store/controller/UserController.java
@@ -3,17 +3,22 @@ package com.example.store.controller;
 import com.example.store.exception.user.AlreadyExistsEmail;
 import com.example.store.request.user.UserCreate;
 import com.example.store.service.user.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final ObjectMapper objectMapper;
 
     @PostMapping("/signup")
     public String signup(@ModelAttribute @Valid UserCreate userCreate){
@@ -24,5 +29,18 @@ public class UserController {
 
         Long userId = userService.signup(userCreate);
         return "redirect:/";
+    }
+
+    @GetMapping("/check-email")
+    public ResponseEntity<String> checkEmail(@RequestParam("email") String email) throws JsonProcessingException {
+        boolean isDuplicate = userService.validateDuplicate(email);
+
+        if (isDuplicate) {
+            String json = objectMapper.writeValueAsString(Map.of("use", "disable"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(json);
+        } else {
+            String json = objectMapper.writeValueAsString(Map.of("use", "available"));
+            return ResponseEntity.status(HttpStatus.OK).body(json);
+        }
     }
 }
