56272e69a60e17dcc35f3360445eea99336ea54b
kimdodo97 <eh4536@naver.com>
2024-06-21 10:45:01 +0900

    Feat : OrderTid 업데이트 추가

---
+++ src/main/java/com/example/store/repository/mapper/OrdersMapper.java
@@ -0,0 +1,20 @@
+package com.example.store.repository.mapper;
+
+import com.example.store.domain.Orders;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
+
+@Mapper
+public interface OrdersMapper {
+    List<Orders> findAll();
+    Orders findById(@Param("orderId") Long orderId);
+    void insertOrder(Orders order);
+    void insertOrderItem(@Param("orderId") Long orderId, @Param("itemId") Long itemId, @Param("itemCount") int itemCount, @Param("totalItemPrice") Long totalItemPrice);
+    void updateOrder(Orders order);
+    void deleteOrder(@Param("orderId") Long orderId);
+    void updateOrderStatus(@Param("orderId") Long orderId, @Param("status") String status);
+    void updateOrderTid(@Param("orderId") Long orderId, @Param("tid") String tid);
+}
