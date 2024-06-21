90e4358fb89079ff513972cbb369a7afa2b02ce3
leedonguk0809 <ehddnr0481@naver.com>
2024-06-21 10:37:03 +0900

    Feat : crat 도메인 추가

--- src/main/java/com/example/store/domain/Cart.java
+++ src/main/java/com/example/store/domain/Cart.java
@@ -1,12 +1,29 @@
 package com.example.store.domain;
 
-import lombok.Data;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
 public class Cart {
-
     private Long cartId;
     private Long userId;
+    private Long itemId;
     private Long totalPrice;
+
+    @Builder
+    public Cart(Long userId, Long itemId, Long totalPrice) {
+        this.userId = userId;
+        this.itemId = itemId;
+        this.totalPrice = totalPrice;
+    }
+
+
+    public void updateTotalPrice(Long totalPrice){
+        this.totalPrice = totalPrice;
+    }
+
+    public void updateItemId(Long itemId){
+        this.itemId = itemId;
+    }
 }
