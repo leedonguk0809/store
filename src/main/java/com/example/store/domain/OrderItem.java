75f2efef25e8c1a67be2debd2e4208aa3ef3ee4c
kimdodo97 <eh4536@naver.com>
2024-06-21 10:42:37 +0900

    Fix : 오더 수정

--- src/main/java/com/example/store/domain/OrderItem.java
+++ src/main/java/com/example/store/domain/OrderItem.java
@@ -1,14 +1,20 @@
 package com.example.store.domain;
 
+import lombok.Builder;
+import lombok.Getter;
 
-import lombok.Data;
-
-
+@Getter
 public class OrderItem {
-
     private Long orderItemId;
     private Long orderId;
     private Long itemId;
+    private Integer count;
 
-
+    @Builder
+    public OrderItem(Long orderItemId, Long orderId, Long itemId, Integer count) {
+        this.orderItemId = orderItemId;
+        this.orderId = orderId;
+        this.itemId = itemId;
+        this.count = count;
+    }
 }
