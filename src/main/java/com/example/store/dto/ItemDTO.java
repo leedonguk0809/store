3011642c93229d51fa5c144dc709f584bb0799ed
kimdodo97 <eh4536@naver.com>
2024-06-21 10:33:07 +0900

    Fix : 아이템 DTO 이미지 필드 추가 및 변환 메서드 추가

--- src/main/java/com/example/store/dto/ItemDTO.java
+++ src/main/java/com/example/store/dto/ItemDTO.java
@@ -1,5 +1,6 @@
 package com.example.store.dto;
 
+import com.example.store.domain.Item;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,4 +17,15 @@ public class ItemDTO {
     private String name;
     private Long price;
     private String info;
+    private  String itemImage;
+
+    public static ItemDTO fromEntity(Item item){
+        return ItemDTO.builder()
+                .itemId(item.getItemId())
+                .name(item.getName())
+                .price(item.getPrice())
+                .info(item.getInfo())
+                .itemImage(item.getItemImage())
+                .build();
+    }
 }
