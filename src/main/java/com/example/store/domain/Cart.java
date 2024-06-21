d6050662df0075ce4b6344b59a34f43e2e625ad1
kimdodo97 <eh4536@naver.com>
2024-06-21 10:43:31 +0900

    Fix : 카트 아이템 생성자 및 수정

--- src/main/java/com/example/store/domain/Cart.java
+++ src/main/java/com/example/store/domain/Cart.java
@@ -1,12 +1,23 @@
 package com.example.store.domain;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.util.List;
 
+
+@Getter
 public class Cart {
-
     private Long cartId;
-    private Long userId;
+    private Long userid;
     private Long totalPrice;
+    private List<CartItem> cartItems;
+    @Builder
+    public Cart(Long cartId, Long userid, Long totalPrice) {
+        this.cartId = cartId;
+        this.userid = userid;
+        this.totalPrice = totalPrice;
+    }
 }
--- src/main/java/com/example/store/domain/CartItem.java
+++ src/main/java/com/example/store/domain/CartItem.java
@@ -1,14 +1,22 @@
 package com.example.store.domain;
 
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
 
 @Getter
 public class CartItem {
-    private Long itemId;
+    private Long id;
     private Long cartId;
-
     private Integer itemCount;
+    private Item item;
+
+    @Builder
+    public CartItem(Long id, Long cartId, Integer itemCount) {
+        this.id = id;
+        this.cartId = cartId;
+        this.itemCount = itemCount;
+    }
 }
