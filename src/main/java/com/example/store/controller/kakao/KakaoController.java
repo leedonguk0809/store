78a4052d520993fd53e50c547fb96c00762c45b4
kimdodo97 <eh4536@naver.com>
2024-06-21 10:35:00 +0900

    Feat : 카카오페이 연동 결제 컨트롤러 구현

---
+++ src/main/java/com/example/store/controller/kakao/KakaoController.java
@@ -0,0 +1,42 @@
+package com.example.store.controller.kakao;
+
+import com.example.store.domain.Payment;
+import com.example.store.request.kakao.KakaoPaymentRequest;
+import com.example.store.request.kakao.ReadyRequest;
+import com.example.store.response.kakao.ReadyResponse;
+import com.example.store.service.KakaoPayService;
+import lombok.RequiredArgsConstructor;
+import org.jboss.jandex.Main;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class KakaoController {
+    private final KakaoPayService payService;
+    @GetMapping("/ready")
+        public String ready(@ModelAttribute KakaoPaymentRequest kakaoPaymentRequest, Model model) {
+        ReadyResponse readyResponse = payService.ready(kakaoPaymentRequest);
+        model.addAttribute("response", readyResponse);
+        return "payment/layer/ready";
+    }
+
+    @GetMapping("/approve")
+    public String approve(@RequestParam("pg_token") String pgToken, @RequestParam("partner_order_id") Long orderId, Model model) {
+        Payment payment = payService.approve(pgToken, orderId);
+        model.addAttribute("payment", payment);
+        return  "payment/approve";
+    }
+
+    @GetMapping("/cancel")
+    public String cancel() {
+        return "payment/layer/cancel";
+    }
+
+    @GetMapping("/fail")
+    public String fail(String openType) {
+        return "payment/layer/fail";
+    }
+}
