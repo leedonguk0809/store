7de5039781f3aba3f9acbd39b469db16e8b23375
kimdodo97 <eh4536@naver.com>
2024-06-21 10:38:09 +0900

    Fix : 시간대 UTC로 고정

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,7 +1,11 @@
 package com.example.store;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class StoreApplication {
@@ -9,5 +13,8 @@ public class StoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
     }
-
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
