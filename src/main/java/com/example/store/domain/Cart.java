065be641d4e313843fa469b4a5faa80d6c8a6d53
kimdodo97 <eh4536@naver.com>
2024-06-21 17:19:06 +0900

    Feat : 장바구니 비지니스 로직 구현

--- src/main/java/com/example/store/domain/Cart.java
+++ src/main/java/com/example/store/domain/Cart.java
@@ -5,19 +5,22 @@ import lombok.Data;
 import lombok.Getter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Getter
 public class Cart {
-    private Long cartId;
-    private Long userid;
-    private Long totalPrice;
-    private List<CartItem> cartItems;
+    private Long id;
+    private Long userId;
+    private int totalPrice;
+    private Set<CartItem> cartItems = new HashSet<>();
     @Builder
-    public Cart(Long cartId, Long userid, Long totalPrice) {
-        this.cartId = cartId;
-        this.userid = userid;
+    public Cart(Long id, Long userId, int totalPrice) {
+        this.id = id;
+        this.userId = userId;
         this.totalPrice = totalPrice;
     }
 }
