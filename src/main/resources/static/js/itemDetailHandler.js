73ea36296ec662b71179be2d48e9bbf2f8cf828a
kimdodo97 <eh4536@naver.com>
2024-06-22 14:21:26 +0900

    Feat : 아이템 상세 화면에서 장바구니 추가 및 alter 추가

--- src/main/resources/static/js/itemDetailHandler.js
+++ src/main/resources/static/js/itemDetailHandler.js
@@ -9,8 +9,6 @@ const addCart = () => {
         "quantity" : parseInt(cartQuantity)
     }
 
-    console.log(json);
-
     fetch('/cart/add', {
         method: 'POST',
         headers: {
@@ -24,10 +22,16 @@ const addCart = () => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
+
             return response.json(); // 서버에서 반환된 JSON 데이터를 읽기
         })
         .then(data => {
-            console.log('서버 응답:', data);
+            // alert 창을 사용하여 장바구니 이동 여부를 묻기
+            if (confirm('장바구니에 추가되었습니다. 장바구니로 이동하시겠습니까?')) {
+                window.location.href = '/cart'; // 장바구니 페이지로 이동
+            } else {
+                alert('쇼핑을 계속하세요.'); // 쇼핑을 계속할 때 메시지
+            }
         })
         .catch(error => {
             console.error('오류 발생:', error);
