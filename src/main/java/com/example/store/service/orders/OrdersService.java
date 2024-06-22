3680ea03dd5da53bf1f08370cae5e47677cf232c
leedonguk0809 <ehddnr0481@naver.com>
2024-06-23 02:16:42 +0900

    Feat : OrdersService 수정

--- src/main/java/com/example/store/service/orders/OrdersService.java
+++ src/main/java/com/example/store/service/orders/OrdersService.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 public interface OrdersService {
     List<Orders> getAllOrders();
-    List<Orders> getOrdersByUserId(Long userId); // 새로운 메서드 추가
+    List<Orders> getOrdersByUserId(Long userId);
     Orders getOrderById(Long id);
     void createOrder(Orders order);
     void updateOrder(Long id, Orders order);
     void deleteOrder(Long id);
+    void updateOrderStatus(Long id, String status);
+    void updateOrderTid(Long id, String tid);
 }
